a5f890c45cc6fe78f5bf68ff82a6bebe746a5898
Philip J MacAoidh <rebledude@me.com>
2017-10-26 18:39:40 +0100

    Q1 Q2 upload

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,6 @@
 #lang racket
+>(cons 1 2)
+>(cons 1 (cons 2 (cons 3 empty)))
+>(cons "ONE" (cons 2 (cons (cons 3 (cons 4 (cons 5 empty))) empty)))
+>(list "ONE" 2 (list 3 4 5))
+>(append '("ONE") '(2) '((3 4 5)))
--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -1,17 +1,45 @@
 #lang racket
 
-;This is an example implementation of ins_beg,
-;It obviously doesn't do what it should, so you
-;can edit this function to get started.
-;
-;Please note the provide function is necessary
-;for the unit tests to work. Please include a
-;(provide) for each function you write in your
-;submitted assignment.
-;
-;You may delete these comments!
+[define (ins_beg num list)
+  (append (cons num empty) list)
+]
 
-(provide ins_beg)
+[define (ins_end num list)
+  (append list (cons num empty))
+]
 
-(define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+[define (cout_top_level list)
+  (cond [(empty? list) 0]
+        [else (+ 1 (cout_top_level (cdr list)))])
+]
+
+[define (count_instances num list)
+  (cond
+    [(empty? list) 0]
+    [(equal? num (car list)) (+ 1 (count_instances num (cdr list)))]
+    [else (count_instances num (cdr list))])
+]
+
+[define (count_instances_tr num list total)
+  (cond
+    [(empty? list) total]
+    [(equal? num (car list)) (count_instances_tr num (cdr list) (+ 1 total))]
+    [else (count_instances_tr num (cdr list) total)])
+]
+
+[define (count_instances_deep num list)
+  (cond
+    [(empty? list) 0]
+    [(equal? num (car list)) (+ 1 (count_instances num (cdr list)))]
+    [(list? (car list)) (+ (count_instances_deep num (car list)) (count_instances_deep num (cdr list)))]
+    [else (count_instances num (cdr list))])
+]
+
+(printf "Q2 A.(a): ~a\n" (ins_beg 'a '(b c d)))
+(printf "Q2 A.(b): ~a\n" (ins_beg '(a b) '(b c d)))
+(printf "Q2 B.(a): ~a\n" (ins_end 'a '(b c d)))
+(printf "Q2 B.(b): ~a\n" (ins_end '(a b) '(b c d)))
+(printf "Q2 C.: ans(4)=~a\n" (cout_top_level '(a b c d)))
+(printf "Q2 D.: ans(2)=~a\n" (count_instances 'z '(p i z z a)))
+(printf "Q2 E.: ans(2)=~a\n" (count_instances_tr 'z '(p i z z a) 0))
+(printf "Q2 F.: ans(4)=~a\n" (count_instances_deep 'd '((a n d r o i d s) (d o n t) (d r e a m))))
